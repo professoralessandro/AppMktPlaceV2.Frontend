dfdd137134d2ed4238e9a92597d399e15d4bdcf6
Alessandro dos Santos <professor_alessandro@hotmail.com>
2023-10-21 10:22:37 -0300

    Criado produto component

---
+++ src/app/cadastros/product/insert-product/insert-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-insert-product',
+  templateUrl: './insert-product.component.html',
+  styleUrls: ['./insert-product.component.scss']
+})
+export class InsertProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
