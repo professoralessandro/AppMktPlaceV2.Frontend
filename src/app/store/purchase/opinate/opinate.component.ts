02314e40b892969ed2bc44095509bfd042ef10e1
Alessandro dos Santos <professor_alessandro@hotmail.com>
2024-05-14 13:26:34 -0300

    shore: added opinate component before implements star-rate component

--- src/app/store/purchase/opinate/opinate.component.ts
+++ src/app/store/purchase/opinate/opinate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-opinate',
@@ -6,10 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./opinate.component.scss']
 })
 export class OpinateComponent implements OnInit {
+  /**
+   * CLASS ATRIBUTTES
+   */
 
-  constructor() { }
+  public stars: number[] = [1, 2, 3, 4, 5];
+  public selectedValue: number = 0;
 
-  ngOnInit(): void {
+  /**
+   * CONSTRUCTOR
+   */
+  constructor(private commonService: CommonService) { }
+
+  /**
+   * PUBLIC METHOD
+   */
+  public ngOnInit(): void {
   }
 
+  public countStar(star) {
+    this.selectedValue = star;
+  }
+
+  public addClass(star) {
+    let ab = "";
+    for (let i = 0; i < star; i++) {
+      ab = "starId" + i;
+      document.getElementById(ab).classList.add("selected");
+    }
+  }
+
+  public removeClass(star) {
+    let ab = "";
+    for (let i = star - 1; i >= this.selectedValue; i--) {
+      ab = "starId" + i;
+      document.getElementById(ab).classList.remove("selected");
+    }
+  }
+
+  public enviarOpiniao(): void {
+    // Lógica para enviar a opinião
+    try {
+      const alertType: string = 'success'
+      const alertMessage: string = 'Seu voto foi salvo com sucesso';
+      this.commonService.responseActionWithoutNavigation(alertType, alertMessage);
+      debugger;
+    }
+    catch(ex) {
+      const alertType: string = 'error';
+      const errorMessage: string = 'Houve um erro ao tentar efetivar o pagamento: \n' + ex.error;
+      this.commonService.responseActionWithoutNavigation(alertType, errorMessage);
+    }
+  }
 }
