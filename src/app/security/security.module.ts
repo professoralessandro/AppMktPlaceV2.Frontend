7a9c923c48efd938239bbb576a159ad86710788d
Alessandro dos Santos <professor_alessandro@hotmail.com>
2024-08-25 11:42:03 -0300

    shore: added component forgot and reset password

--- src/app/security/security.module.ts
+++ src/app/security/security.module.ts
@@ -4,10 +4,14 @@ import { LoginComponent } from './user/login/login.component';
 import { LoginRoutingModule } from './user/login/login.routing';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ForgotPasswordComponent } from './user/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './user/reset-password/reset-password.component';
 
 @NgModule({
     declarations: [
       LoginComponent,
+      ForgotPasswordComponent,
+      ResetPasswordComponent,
     ],
     imports: [
         CommonModule,
