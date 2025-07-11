2234161297867b9dddb2853ac13d65c63cb4cf3b
Alessandro dos Santos <professor_alessandro@hotmail.com>
2025-07-11 17:16:25 -0300

    feature: alterada porta da API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   production: false,
-  store_url: 'http://localhost:5020',
-  security_url: 'http://localhost:5030',
+  start_url: 'http://localhost:5010',
+  security_url: 'http://localhost:5020',
+  store_url: 'http://localhost:5030',
   storage_url: 'http://localhost:5040',
   cadastros_url: 'http://localhost:5050',
-  notifications_url: 'http://localhost:5050',
-  start_url: 'http://localhost:5010',
+  notifications_url: 'http://localhost:5060',
 };
 
 /*
